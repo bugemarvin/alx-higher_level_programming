e41a4f8eaa2661c407c95ec98cdd7617c577737e
Marvin Kurland <bugemarvin@outlook.com>
2022-08-01 07:28:00 +0300

    initial commit for task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+'''
+function that returns available attributes
+and methods of an object.
+'''
+
+def lookup(obj):
+        '''prototype for the object
+        '''
+        pass
+
+lookup()
