498d069b5d46a7bd0843c15ac960076516dcbbde
Marvin Kurland <bugemarvin@outlook.com>
2022-08-05 00:59:28 +0300

    initial commit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+program will assign a random signed number to the variable number
+'''
+
+import random
+number = random.randint(-10, 10)
+
+
+if number > 0:
+    print(f'{number} is positive')
+elif number == 0:
+    print(f'{number} is zero')
+else:
+    print(f'{number} is negative')
