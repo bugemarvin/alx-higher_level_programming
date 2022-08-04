028adbcb2203f02430942f6cf3aceb184d8db465
Marvin Kurland <bugemarvin@outlook.com>
2022-08-05 02:35:11 +0300

    added format specifier

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,13 +3,6 @@
 program that prints the ASCII alphabet, in lowercase.
 '''
 
+for alpha in range(97, 123):
+    print("{}".format(chr(alpha)), end='')
 
-def lower_case():
-    '''
-    class for lower case
-    '''
-    for alpha in range(97, 123):
-        print("{}".format(chr(alpha)), end='')
-
-
-lower_case()
