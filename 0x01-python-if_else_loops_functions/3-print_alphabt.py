2d3766582666b3fc2cb109f9a71d19cbe41b25af
Marvin Kurland <bugemarvin@outlook.com>
2022-08-05 03:17:34 +0300

    ASCII in lower case without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -5,6 +5,6 @@ In lowercase, not followed by a new line.
 Print all the letters except q and e.
 '''
 
-for alpha in range(97, 122):
+for alpha in range(97, 123):
     if alpha != 101 and alpha != 113:
         print('{}'.format(chr(alpha)), end='')
