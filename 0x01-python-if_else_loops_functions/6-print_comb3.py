8feabf02096989214cbb9acf9f681c1cf365fa76
Marvin Kurland <bugemarvin@outlook.com>
2022-08-11 22:56:59 +0300

    print combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for a in range(0, 10):
     for b in range(a + 1, 10):
         if a != b:
-            if a == 8 and b == 9:         
+            if a == 8 and b == 9:
                 print('{}{}'.format(a, b))
             else:
                 print('{}{}'.format(a, b), end=', ')
