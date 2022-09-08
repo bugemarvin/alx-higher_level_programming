e754856d732cffd55b247233c7be51d06f2e337c
Marvin Kurland <bugemarvin@outlook.com>
2022-09-08 22:27:12 +0300

    lists the tables for hbtn.. table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM first_table FROM IF NOT EXISTS hbtn_0c_0;
+SHOW COLUMNS FROM first_table;
