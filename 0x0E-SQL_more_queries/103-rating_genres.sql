814a1528ccd58f010e6c8bfb2bdc52cce4d3776d
Marvin Kurland <bugemarvin@outlook.com>
2022-09-09 00:48:38 +0300

    -

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,1 +1,3 @@
 -- Import the database dump from hbtn_0d_tvshows_rate to your MySQL server: download (same as 102-rating_shows.sql)
+SELECT `tv_genres_name`, `rating.COUNT(*)`
+FROM `hbtn_0d_tvshows_rate`
