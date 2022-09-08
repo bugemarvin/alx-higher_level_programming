02f38ac6161be19b62801d3310b70be47457f0f7
Marvin Kurland <bugemarvin@outlook.com>
2022-09-08 23:05:57 +0300

    trying to display items

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
 -- script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT * FROM frist_table
-WHERE `id` LIKE '89';
+SELECT * FROM frist_table WHERE id = '89';
