5b4db0e99b858a0057921e1d3c57ab0e4ba870f4
Marvin Kurland <bugemarvin@outlook.com>
2022-09-08 22:50:42 +0300

    adding a new row

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-FROM *
-INSERT INTO first_table (id INT, name VARCHAR(256))
-VALUES (89, Best School)
+FROM * 
+INSERT INTO first_table (id INT, name VARCHAR(256)) 
+VALUES (89, Best School);
