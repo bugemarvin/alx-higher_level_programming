f8a06e440241332750afe3905c7810b0b7bba776
Marvin Kurland <bugemarvin@outlook.com>
2022-10-04 01:55:03 +0300

    **

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const args = process.argv.length;
-if (args < 3) {
-  console.log('No argument');
-}
-else if (args < 4) {
-  console.log('Argument found');
-}
-else {
-  console.log('Arguments found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
