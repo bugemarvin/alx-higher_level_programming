8524ee93042912311c76d9d83c4fbf94b5e5bd16
marvin buge <bugemarvin@outlook.com>
2022-10-05 14:58:04 +0300

    fix reectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-        constructor (width, height) {
-                this.width = width;
-                this.height = height;
-                return width * height;
-        }
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
 }
