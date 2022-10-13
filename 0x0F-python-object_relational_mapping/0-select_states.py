384dc6babf369483840f54d2f90391810d5bb950
Marvin Buge <bugemarvin@outlook.com>
2022-10-13 21:19:08 +0000

    replit tool

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,17 @@
 """
 
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
+	import sys
+	import MySQLdb
 
-    db_rows = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+	db_rows = MySQLdb.connect(host='localhost',
+	                          port=3306,
+	                          user=sys.argv[1],
+	                          passwd=sys.argv[2],
+	                          db=sys.argv[3])
 
-    base_connector = db_rows.cursor()
-    base_connector.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    db_rows = base_connector.fetchall()
-    for info_row in db_rows:
-        print(info_row)
+	base_connector = db_rows.cursor()
+	base_connector.execute("SELECT * FROM states ORDER BY states.id ASC;")
+	db_rows = base_connector.fetchall()
+	for info_row in db_rows:
+		print(info_row)
