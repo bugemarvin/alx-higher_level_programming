7f841c101d5195fed015d1e22598b90d41619743
Marvin Kurland <bugemarvin@outlook.com>
2022-10-14 12:27:36 +0300

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that takes info from user and displays the result from the database
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    base_connector = MySQLdb.connect(host='localhost', port=3306,
+                                     user=sys.argv[1], passwd=sys.argv[2],
+                                     db=sys.argv[3])
+    states_query = sys.argv[4]
+    db_connected = base_connector.cursor()
+    db_connected.execute("SELECT * FROM states WHERE name LIKE '{}';".format(states_query))
+    base_connector = db_connected.fetchall()
+    for info in base_connector:
+        print(info)
