5b3f8ecbde31d173f739a6e5aa0cb8da68ecfd92
Marvin Kurland <bugemarvin@outlook.com>
2022-10-14 12:31:27 +0300

    script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                                      db=sys.argv[3])
     states_query = sys.argv[4]
     db_connected = base_connector.cursor()
-    db_connected.execute("SELECT * FROM states WHERE name LIKE '{}';"
+    db_connected.execute("SELECT * FROM states WHERE CAST(name AS BINARY) LIKE '{}';"
                          .format(states_query))
     base_connector = db_connected.fetchall()
     for info in base_connector:
