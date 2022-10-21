761c44b3a1a891d9c3e92cd875c0bda850c13262
Marvin Kurland <bugemarvin@outlook.com>
2022-10-21 23:01:17 +0300

    displays all HTTP methos that can be passed to a server

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Request method that accepts all the HTTP server URL requests
-curl -sIHd $1 -L | grep "Allow:" | cut -d " " -f 2-4
+curl -s --head $1 -L | grep "Allow:" | cut -d " " -f 2-4
