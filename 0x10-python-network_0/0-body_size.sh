4018be90d519ccd84257c234dcad42f7571af37e
Marvin Kurland <bugemarvin@outlook.com>
2022-10-21 17:43:28 +0300

    displaying the Content-Length for a resource

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL
-# displays the size of the body of the response
+# script that takes in a URL, outputs the length.
 curl -s --head $1 | grep "Content-Length:" | cut -d " " -f '2' 
 
