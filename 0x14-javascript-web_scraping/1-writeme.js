b6ca684da0273b3272e598a4fd61eeb10579c45d
Marvin Kurland <bugemarvin@outlook.com>
2022-11-16 00:58:33 +0300

    append text to file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-let info = process.argv[3];
+const info = process.argv[3];
 fs.writeFile(process.argv[2], info, 'utf-8', (err) => {
-	console.error(err);
+  console.error(err);
 });
