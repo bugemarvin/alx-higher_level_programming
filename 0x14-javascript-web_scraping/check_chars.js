7a04bd476f84b2ce4af83cf44116575397e634f4
Marvin Kurland <bugemarvin@outlook.com>
2022-11-16 03:02:21 +0300

    script that gets the contents of a webpage and stores it in a file.

--- 0x14-javascript-web_scraping/check_chars.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-request.get(`https://swapi-api.hbtn.io/api/people/${process.argv[2]}`, (err, req, res) => {
-  if (err) throw err;
-  const chars = 'https://swapi-api.hbtn.io/api/people/'.filter(process.argv[2]);
-  for (let i = 0; i <= chars; i++) {
-    console.log(i);
-  }
-});
