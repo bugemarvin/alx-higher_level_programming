a8d15fd413de8b6da084b598de5fee62a8a44640
Marvin Kurland <bugemarvin@outlook.com>
2022-11-23 23:07:12 +0300

    a JavaScript script that updates the text color

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,5 @@
 let texter = '';
-if (typeof document !== "undefined") {
-	texter = document.querySelector('header').style.color = "#FF0000";
+if (typeof document !== 'undefined') {
+  texter = document.querySelector('header').style.color = '#FF0000';
 }
 console.log(texter);
