471185d372d480ff8b057d34efc259f3cb828149
Marvin Kurland <bugemarvin@outlook.com>
2022-11-24 01:07:03 +0300

    a JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+/* a JavaScript script that updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header */
+const $ = window.$;
+$('#update_header').click(() => {
+  $('header').text('New Header!!!');
+});
