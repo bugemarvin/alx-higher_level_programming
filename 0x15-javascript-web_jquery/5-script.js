b34162c5cc0bf9bdbff1af56cdd6af57bd5f74bf
Marvin Kurland <bugemarvin@outlook.com>
2022-11-24 00:54:31 +0300

    a JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,5 @@
+/* a JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item */
+const $ = window.$;
+$('#add_item').click(() => {
+  $('.my_list').append('<li>Item</li>');
+});
